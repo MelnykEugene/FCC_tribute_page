d951ca72b9e45ac6706fc393993ac2c3bc6c90d7
Yevhen Melnyk <yevhenmelnyk@host-Yevhens-MBP-mac-8c85903ae7ba.eduroam.ssid.10net.amherst.edu>
2020-08-07 16:21:27 -0400

    figured out fade-in animation

---
+++ fadein.js
@@ -0,0 +1,11 @@
+$("#hero-image").animate(
+  {
+    backgroundImage:
+      "linear-gradient(125deg, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)), url('board.jpg')",
+  },
+  {
+    duration: 1000,
+    delay: 250,
+    easing: "linear",
+  }
+);
